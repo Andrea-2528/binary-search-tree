e704fd2c6dd6a8248a087a738b222b75026115b7
Andrea-2528 <188598696+Andrea-2528@users.noreply.github.com>
2025-01-19 18:04:42 +0100

    Add first methods

--- bst.js
+++ bst.js
@@ -31,6 +31,15 @@ class Tree {
 	sortArray() {}
 	insertItem() {}
 	deleteItem() {}
+	find() {}
+	levelOrder() {}
+	inOrder() {}
+	preOrder() {}
+	postOrder() {}
+    height() {}
+    depth() {}
+    isBalanced() {}
+    rebalance() {}
 
 	buildTree(array, start, end) {
 		if (start > end) {
@@ -56,7 +65,129 @@ class Tree {
 		return unique;
 	}
 
-	insertItem(value) {}
+	insertItem(value) {
+		const insertRecursive = (node, value) => {
+			if (!node) {
+				return new Node(value);
+			}
+			if (value === node.data) {
+				return node;
+			}
+			if (value < node.data) {
+				node.left = insertRecursive(node.left, value);
+			} else if (value > node.data) {
+				node.right = insertRecursive(node.right, value);
+			}
+			return node;
+		};
+		this.root = insertRecursive(this.root, value);
+	}
 
-	deleteItem(value) {}
+	deleteItem(value) {
+		const getSuccessor = (curr) => {
+			curr = curr.right;
+			while (curr !== null && curr.left !== null) {
+				curr = curr.left;
+			}
+			return curr;
+		};
+
+		const deleteNode = (node, value) => {
+			if (node === null) {
+				return node;
+			}
+			if (node.data > value) {
+				node.left = deleteNode(node.left, value);
+			} else if (node.data < value) {
+				node.right = deleteNode(node.right, value);
+			} else {
+				if (node.left === null) {
+					return node.right;
+				}
+				if (node.right === null) {
+					return node.left;
+				}
+				let successor = getSuccessor(node);
+				node.data = successor.data;
+				node.right = deleteNode(node.right, successor.data);
+			}
+			return node;
+		};
+
+		this.root = deleteNode(this.root, value);
+	}
+
+	find(value) {
+		const findRecursive = (node, value) => {
+			if (!node) {
+				return null;
+			}
+			if (value === node.data) {
+				return node;
+			}
+			if (value < node.data) {
+				return findRecursive(node.left, value);
+			} else if (value > node.data) {
+				return findRecursive(node.right, value);
+			}
+		};
+		return findRecursive(this.root, value);
+	}
+
+	levelOrder() {
+		const queue = [];
+		const result = [];
+		queue.push(this.root);
+		while (queue.length !== 0) {
+			let node = queue.shift();
+			result.push(node.data);
+			if (node.left) {
+				queue.push(node.left);
+			}
+			if (node.right) {
+				queue.push(node.right);
+			}
+		}
+		return result;
+	}
+
+    
+    inOrder() {
+        const result = [];
+        const traverse = (node) => {
+            if (node !== null) {
+                traverse(node.left);
+                result.push(node.data);
+                traverse(node.right);
+            }
+        };
+        traverse(this.root);
+        return result;
+    }
+
+    preOrder() {
+        const result = [];
+        const traverse = (node) => {
+            if (node !== null) {
+                result.push(node.data);
+                traverse(node.left);
+                traverse(node.right);
+            }
+        };
+        traverse(this.root);
+        return result;
+    }
+
+    postOrder() {
+        const result = [];
+        const traverse = (node) => {
+            if (node !== null) {
+                traverse(node.left);
+                traverse(node.right);
+                result.push(node.data);
+            }
+        };
+        traverse(this.root);
+        return result;
+    }
 }
--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 import { Tree } from "./bst.js";
-const testArray = [
-	1, 43, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 324, 1, 43, 4, 23, 8, 9, 4, 3, 5, 7,
-	9, 67, 324, 32, 78, 42, 12,
-];
-const tree = new Tree(testArray);
+// const testArray = [
+// 	1, 43, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 324, 1, 43, 4, 23, 8, 9, 4, 3, 5, 7,
+// 	9, 67, 324, 32, 78, 42, 12,
+// ];
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
 	if (node === null) {
@@ -18,5 +17,49 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 	}
 };
 
+const tree = new Tree([10, 8, 32, 9, 17, 82, 24, 21, 5, 1, 4, 7]);
+
+// TEST
+console.log("TESTS -------------------------------------------------------------");
+// Insert test
+console.log("Insert test -------------------------------------------------------------");
+tree.insertItem(12);
+tree.insertItem(62);
+tree.insertItem(6);
+tree.insertItem(2);
+tree.insertItem(98);
 prettyPrint(tree.root);
 
+// Delete test
+console.log("Delete test -------------------------------------------------------------");
+tree.deleteItem(2);
+tree.deleteItem(7);
+tree.deleteItem(9);
+prettyPrint(tree.root);
+
+// Find test
+console.log("Find test -------------------------------------------------------------");
+console.log(" 24 -------------------------------------------------------------");
+console.log(tree.find(24));
+console.log(" 17 -------------------------------------------------------------");
+console.log(tree.find(17));
+console.log(" 10 -------------------------------------------------------------");
+console.log(tree.find(10));
+
+
+prettyPrint(tree.root);
+// Level order test
+console.log("Level order test -------------------------------------------------------------");
+console.log(tree.levelOrder());
+
+// In order test
+console.log("In order test -------------------------------------------------------------");
+console.log(tree.inOrder());
+
+// Pre order test
+console.log("Pre order test -------------------------------------------------------------");
+console.log(tree.preOrder());
+
+// Post order test
+console.log("Post order test -------------------------------------------------------------");
+console.log(tree.postOrder());
